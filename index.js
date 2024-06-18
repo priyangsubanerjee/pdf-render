f343d6d2a7d3dee8927e36008acd6a5674e2b78e
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-06-18 13:12:47 +0530

    chore: Update static file serving in index.js
    The code changes update the way static files are served in the `index.js` file. Instead of using a hardcoded path, the `express.static` middleware now uses the `path.join` function to dynamically serve the `public` folder. This change improves the flexibility and maintainability of the code.
    
    The commit message suggests using the `chore` type to indicate that this is a non-functional change that improves the development process. The message itself is concise and describes the purpose of the code changes.

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use("public", express.static("public"));
+// statically serve the public folder using path.join
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
