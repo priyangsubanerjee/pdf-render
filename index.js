c08279774452d74986e8e5d9481d1231e729d033
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-06-18 13:06:21 +0530

    initializing

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use("public", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/file", (req, res) => {
+  const dir = "public";
+  const file = path.join(dir, "hello.txt");
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  fs.open(file, "w", function (err, file) {
+    if (err) throw err;
+    console.log("File is opened in write mode.");
+  });
+
+  fs.writeFile(file, new Date().toLocaleString(), function (err) {
+    if (err) throw err;
+  });
+
+  res.send("File created!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
